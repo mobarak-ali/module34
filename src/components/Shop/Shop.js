f6300be2605b8837e369486c998b5009c7af30c0
Mobarak Ali <mobarak.ali@outlook.com>
2021-03-09 04:02:45 +0600

    lesson 12

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,22 +8,27 @@ const Shop = () => {
 
     const first10 = fakeData.slice(0, 10);
     const [products] = useState(first10);
+    const [cart, setCart] = useState([]);
 
     const handleAddProduct = (product) =>{
         console.log("Product Added!", product);
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
 
     return (
         <div className="shop-contaienr">
             <div className="product-container">
-                <h1>Shop</h1>
+                <h2>Shop</h2>
                 {
                     products.map(product => <Product handleAddProduct = {handleAddProduct}  product = {product}></Product>)
                 }
 
             </div>
             <div className="cart-container">
-                <h3>Cart Container</h3>
+                <h2>Order Summery</h2>
+                <h5>Items Ordered {cart.length}</h5>
+
 
             </div>
         </div>
